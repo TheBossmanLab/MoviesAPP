f1bb8ecda08ba6915af0205d451a70203d05eb0a
ossman rauf <ossmanrauf8@gmail.com>
2021-07-29 22:16:43 +0200

    add gitignore

--- src/components/AllMovies.js
+++ src/components/AllMovies.js
@@ -3,16 +3,7 @@ function AllMovies({ data }) {
 	return (
 		<div className="px-1 grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3 place-items-center">
 			{data.results.map((movie) => {
-				return (
-					<Singlemovie
-						movieInfo={{
-							posterUrl: movie.poster_path,
-							rating: movie.vote_average,
-							title: movie.title,
-						}}
-						key={movie.id}
-					/>
-				);
+				return <Singlemovie movieInfo={movie} key={movie.id} />;
 			})}
 		</div>
 	);
--- src/components/SingleMovie.js
+++ src/components/SingleMovie.js
@@ -2,23 +2,27 @@ import star from "./star.png";
 
 function Singlemovie({ movieInfo }) {
 	const basePosterUrl = "https://image.tmdb.org/t/p/w500";
-	const { title, posterUrl, rating } = movieInfo;
+	const { title, poster_path, rating, backdrop_path } = movieInfo;
 	// console.log(`title ${title}, path ${posterUrl}, rating${rating}`);
-	// console.log(movieInfo);
+	console.log(movieInfo);
 	return (
 		<div className=" text-sm whitespace-pre-line text-left text-black moviecard shadow-sm bg-white rounded-xl m-4">
 			<a href="www.google.com" className="movie">
 				<figure className="">
 					<img
 						className="rounded-t-xl movie-img"
-						src={basePosterUrl + posterUrl}
+						src={
+							poster_path
+								? basePosterUrl + poster_path
+								: "https://www.movienewz.com/wp-content/uploads/2014/07/poster-holder.jpg"
+						}
 						alt="poster"
 						width="350"
 						height="450"
 					/>
 				</figure>{" "}
-				<p className="pt-2 font-medium px-2 link text-black">{title}</p>
-			</a>
+				<p className="pt-2 font-medium px-2 link text-black"> {title} </p>{" "}
+			</a>{" "}
 			<span className="pb-2 text-black font-semibold flex items-center justify-end pr-4">
 				<p className="inline-block text-right text-sm px-3"> {rating} </p>{" "}
 				<img
