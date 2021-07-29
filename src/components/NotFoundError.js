c93a653e14d5e6d0701c7035f043312a863bfac2
ossman rauf <ossmanrauf8@gmail.com>
2021-07-30 00:07:20 +0200

    adding styles in 404 page

--- src/components/NotFoundError.js
+++ src/components/NotFoundError.js
@@ -1,9 +1,14 @@
-function NotFoundError({ pagesNr, pageUrl }) {
+function NotFoundError() {
 	return (
-		<div>
-			<h1>ERROR:404</h1>
-			<h2>Not Found</h2>
-			<h6>Something is wrong this page doesn't exist in our server</h6>
+		<div class="flex h-screen">
+			<div class="text-center m-auto mt-40">
+				<h1 className="text-2xl font-bold my-3"> ERROR: 404 </h1>{" "}
+				<h2 className="text-xl my-3 font-semibold"> Not Found </h2>{" "}
+				<h6 className="text-xl my-3 font-semibold">
+					{" "}
+					Something is wrong this page doesn't exist in our server
+				</h6>{" "}
+			</div>
 		</div>
 	);
 }
